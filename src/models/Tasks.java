86182155d0cebbdd052da7b9c1d80d6f238e0f07
宮島潤 <@LAPTOP-SEEH68DM>
2022-03-09 15:56:59 +0900

    getAllMessagesを追加、modelの名称修正

--- src/models/Tasks.java
+++ src/models/Tasks.java
@@ -7,11 +7,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "messages")
-public class Message {
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllMessages",
+        query = "SELECT t FROM Tasks AS t ORDER BY t.id DESC"
+    )
+})
+@Table(name = "tasks")
+public class Tasks {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
